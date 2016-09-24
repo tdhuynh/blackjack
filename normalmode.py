22b1cfdd2c022f84e1d5b42a71ccfe903caa8cb6
tdhuynh <tommyhuynh93@gmail.com>
2016-09-24 17:42:23 -0400

    game working, need to clean up

--- normalmode.py
+++ normalmode.py
@@ -1,14 +1,13 @@
-# dealer shuffles
-# deal 1 card at a time, total of two per player
-# dealer's second card is hidden
-# check for 21 after deal
-# each time a hit happens, check for win
 
+# dealer's second card is hidden
+
+import sys
 from random import shuffle
 
 suits = ('Clubs', 'Spades', 'Diamonds', 'Hearts')
 characters = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
 values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
+
 
 class Person:
     def __init__(self, name):
@@ -33,8 +32,19 @@ class Person:
     def check_blackjack_or_bust(self):
         if 21 == self.check_values():
             print("Blackjack! {} wins!".format(self.name))
+            sys.exit()
         elif 21 < self.check_values():
             print("Bust! {} loses!".format(self.name))
+            sys.exit()
+
+    def get_info(self):
+        print(self.name)
+        print(self.hand)
+        print(self.check_values())
+
+    def partial_info(self):
+        print(self.name)
+        print(self.hand[0])
 
 
 class Deck:
@@ -51,16 +61,10 @@ class Deck:
 def check_win(player, dealer):
     if 21 >= player.check_values() > dealer.check_values():
         print("Player wins!")
+        sys.exit()
     elif 21 >= dealer.check_values() > player.check_values():
         print("Dealer wins!")
-
-
-# class Hand:
-    # def get_score(self):
-    #     value = 0
-    #     for card in self.hand:
-    #         value += card[1]
-    #     return value
+        sys.exit()
 
 
 player = Person("Player")
@@ -70,24 +74,22 @@ deck.shuffle_deck()
 for i in range(2):
     dealer.add_card_to_hand(deck.deal_card())
     player.add_card_to_hand(deck.deal_card())
-print(dealer.hand, player.hand)
+player.get_info()
+dealer.partial_info()
 player.check_blackjack_or_bust()
-dealer.check_blackjack_or_bust()
 
 
-while True:
+while player.check_values() < 21 and dealer.check_values() < 21:
     hit_or_stand = input("[H]it or [S]tand? ")
     if hit_or_stand.upper() == 'H':
         player.add_card_to_hand(deck.deal_card())
-        print(player.hand)
-        print(player.check_values())
+        player.get_info()
         player.check_blackjack_or_bust()
     else:
-        # check to see who wins
-        while dealer.check_values() <= 16:
+        dealer.get_info()
+        dealer.check_blackjack_or_bust()
+        while dealer.check_values() < 17:
             dealer.add_card_to_hand(deck.deal_card())
-            print(dealer.hand)
-            print(dealer.check_values())
+            dealer.get_info()
             dealer.check_blackjack_or_bust()
-
         check_win(player, dealer)
